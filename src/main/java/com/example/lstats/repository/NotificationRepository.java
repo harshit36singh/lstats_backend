392a70ebb6b61df726b3a26b5f9cda85c9b437f7
HarshitSingh3636 <singh36harshit@gmail.com>
2025-10-22 12:48:33 +0530

    added notification

--- src/main/java/com/example/lstats/repository/NotificationRepository.java
+++ src/main/java/com/example/lstats/repository/NotificationRepository.java
@@ -8,5 +8,5 @@ import com.example.lstats.model.Notification;
 import com.example.lstats.model.User;
 
 public interface NotificationRepository extends JpaRepository<Notification,Long>{
-    List<Notification> findReceiverAndReadStatusFalse(User receiver);
+    List<Notification> findByReceiverAndReadStatusFalse(User receiver);
 }
